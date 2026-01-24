32df9c40ae68a30a3d3d4e83d6889624a8b75aa2
xilingwanyan <xilingwanyan@163.com>
2026-01-24 19:52:58 +0800

    sync 2026-01-24 19:52:58

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -16,7 +16,6 @@ vim.opt.list = true
 vim.opt.shell = "/data/data/com.termux/files/usr/bin/bash"
 vim.opt.clipboard = ""
 vim.opt.numberwidth = 4
-vim.g.autoformat = false
 vim.opt.listchars = {
 	tab = "> ",
 	trail = "-",

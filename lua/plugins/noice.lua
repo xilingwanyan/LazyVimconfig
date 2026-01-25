b6e39072155bf129ac2b63d0fdbc8fe96c018c2f
xilingwanyan <xilingwanyan@163.com>
2026-01-25 12:31:18 +0800

    sync 2026-01-25 12:31:18

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -1,44 +1,26 @@
--- return {
---     {
---         "folke/noice.nvim",
---         enabled = false,
---     },
--- }
--- return {
---   {
---     "folke/noice.nvim",
---     opts = {
---       cmdline = {
---         enabled = true,
---         view = "cmdline_popup",
---       },
---     },
---   },
--- }
 return {
-	{
-		"folke/noice.nvim",
-		opts = {
-			cmdline = {
-				view = "cmdline_popup",
-			},
-			views = {
-				cmdline_popup = {
-					position = {
-						row = -1,
-						col = 0,
-					},
-					size = {
-						min_width = 0,
-					},
-					border = {
-						style = "none",
-					},
-					win_options = {
-						winhighlight = { Normal = "CursorLine" },
-					},
-				},
-			},
-		},
+    "folke/noice.nvim",
+    opts = {
+        cmdline = {
+            view = "cmdline_popup",
+        },
+        views = {
+            cmdline_popup = {
+                position = {
+                    row = -1,
+                    col = 0,
+                },
+                size = {
+                    min_width = 0,
+                },
+                border = {
+                    style = "none",
+                },
+                win_options = {
+                    winhighlight = { Normal = "CursorLine" },
+                },
+            },
+        },
 	},
 }
+
--- lua/plugins/yanky.lua
+++ lua/plugins/yanky.lua
@@ -4,7 +4,7 @@ return {
   "gbprod/yanky.nvim",
   keys = {
     {
-      "<leader>p",
+      "<C-p>",
       function()
         if LazyVim.pick.picker.name == "telescope" then
           require("telescope").extensions.yank_history.yank_history({})
@@ -20,24 +20,6 @@ return {
 
     { "<leader>p", '"+p', mode = { "n", "x" }, desc = "Paste from Clipboard" },
 
-        -- stylua: ignore
-    { "y", "<Plug>(YankyYank)", mode = { "n", "x" }, desc = "Yank Text" },
-    { "p", "<Plug>(YankyPutAfter)", mode = { "n", "x" }, desc = "Put Text After Cursor" },
-    { "P", "<Plug>(YankyPutBefore)", mode = { "n", "x" }, desc = "Put Text Before Cursor" },
-    { "gp", "<Plug>(YankyGPutAfter)", mode = { "n", "x" }, desc = "Put Text After Selection" },
-    { "gP", "<Plug>(YankyGPutBefore)", mode = { "n", "x" }, desc = "Put Text Before Selection" },
-    { "[y", "<Plug>(YankyCycleForward)", desc = "Cycle Forward Through Yank History" },
-    { "]y", "<Plug>(YankyCycleBackward)", desc = "Cycle Backward Through Yank History" },
-    { "]p", "<Plug>(YankyPutIndentAfterLinewise)", desc = "Put Indented After Cursor (Linewise)" },
-    { "[p", "<Plug>(YankyPutIndentBeforeLinewise)", desc = "Put Indented Before Cursor (Linewise)" },
-    { "]P", "<Plug>(YankyPutIndentAfterLinewise)", desc = "Put Indented After Cursor (Linewise)" },
-    { "[P", "<Plug>(YankyPutIndentBeforeLinewise)", desc = "Put Indented Before Cursor (Linewise)" },
-    { ">p", "<Plug>(YankyPutIndentAfterShiftRight)", desc = "Put and Indent Right" },
-    { "<p", "<Plug>(YankyPutIndentAfterShiftLeft)", desc = "Put and Indent Left" },
-    { ">P", "<Plug>(YankyPutIndentBeforeShiftRight)", desc = "Put Before and Indent Right" },
-    { "<P", "<Plug>(YankyPutIndentBeforeShiftLeft)", desc = "Put Before and Indent Left" },
-    { "=p", "<Plug>(YankyPutAfterFilter)", desc = "Put After Applying a Filter" },
-    { "=P", "<Plug>(YankyPutBeforeFilter)", desc = "Put Before Applying a Filter" },
   },
 }
 

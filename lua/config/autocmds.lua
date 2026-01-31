1f32edd590762f0ab06ff3ef99bb3262d728986e
xilingwanyan <xilingwanyan@163.com>
2026-01-31 17:00:37 +0800

    sync 2026-01-31 17:00:37

--- lua/config/autocmds.lua
+++
@@ -1,5 +0,0 @@
-vim.api.nvim_set_hl(0, "SnacksDashboardHeader", { fg = "#64B5F6"})
-vim.api.nvim_set_hl(0, "SnacksDashboardFooter", { fg = "#BA68C8"})
-vim.api.nvim_set_hl(0, "SnacksDashboardValue", { fg = "#4FC3F7"})
-vim.api.nvim_set_hl(0, "SnacksDashboardSpecial", { fg = "#E5C07B"})
-

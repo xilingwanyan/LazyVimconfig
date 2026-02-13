1bc976321626cbe935754ff5ca36b56a0122d203
xilingwanyan <xilingwanyan@163.com>
2026-02-13 20:39:26 +0800

    sync 2026-02-13 20:39:26

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -152,6 +152,26 @@ return {
                     LazyVim.lualine.root_dir(),
                     { "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 } },
                     { LazyVim.lualine.pretty_path() },
+                },
+                lualine_x = {
+                    {
+                        "diff",
+                        symbols = {
+                            added = icons.git.added,
+                            modified = icons.git.modified,
+                            removed = icons.git.removed,
+                        },
+                        source = function()
+                            local gitsigns = vim.b.gitsigns_status_dict
+                            if gitsigns then
+                                return {
+                                    added = gitsigns.added,
+                                    modified = gitsigns.changed,
+                                    removed = gitsigns.removed,
+                                }
+                            end
+                        end,
+                    },
                 }
             },
         }

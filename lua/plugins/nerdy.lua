9c19dcc05baf73a6ac9b6560d25f2b2a92be1d19
xilingwanyan <xilingwanyan@163.com>
2026-07-20 05:37:54 +0800

    sync 2026-07-20 05:37:54

---
+++ lua/plugins/nerdy.lua
@@ -0,0 +1,16 @@
+return {
+    "2kabhishek/nerdy.nvim",
+    dependencies = {
+        "folke/snacks.nvim",
+    },
+    cmd = "Nerdy",
+    opts = {
+        max_recents = 40, -- Configure recent icons limit
+        copy_to_clipboard = false, -- Copy glyph to clipboard instead of inserting
+        copy_register = "+", -- Register to use for copying (if `copy_to_clipboard` is true)
+    },
+    keys = {
+        { "<leader>in", "<cmd>Nerdy list<CR>", desc = "Browse nerd icons" },
+        { "<leader>iN", "<cmd>Nerdy recents<CR>", desc = "Browse recent nerd icons" },
+    },
+}
